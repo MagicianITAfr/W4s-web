f440d1689b3c3d27a65ac5406b70b0fd4bc167c8
mathias ulano <ulano.mathias@web4student.it>
2026-03-24 17:16:42 +0000

    index

--- processargv/calc.js
+++ processargv/calc.js
@@ -12,5 +12,10 @@ else if (process.argv[3] === "mol") {
 }
 //divisione
 else if (process.argv[3] === "div") {
+	if(process.argv[4] == 0){
+	console.log("divisione per 0")
+	}
+  else{
   console.log(Number(process.argv[2]) / Number(process.argv[4]));
+}
 }
