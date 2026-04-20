090f5f0497f25c0497ad58220c785e1689469fff
mathias ulano <ulano.mathias@web4student.it>
2026-04-20 21:45:40 +0200

    es06a

---
+++ es06/calc.js
@@ -0,0 +1,26 @@
+function calcola() {
+    // process.argv[0] è il percorso di node
+    // process.argv[1] è il percorso del file
+    const n1 = Number(process.argv[2]);
+    const n2 = Number(process.argv[4]);
+    const op = process.argv[3];
+
+    if (isNaN(n1) || isNaN(n2)) {
+        console.log("Errore: uno dei valori non �un numero.");
+        return;
+    }
+
+    let risultato;
+    switch (op) {
+        case "+":  risultato = n1 + n2; break;
+        case "-":  risultato = n1 - n2; break;
+        case "*":  risultato = n1 * n2; break;
+        case "/":  risultato = n2 !== 0 ? n1 / n2 : "Errore: div per 0"; break;
+        case "**": risultato = n1 ** n2; break;
+        default:   risultato = "Errore: operazione non valida.";
+    }
+
+    console.log("Risultato:", risultato);
+}
+
+calcola();
